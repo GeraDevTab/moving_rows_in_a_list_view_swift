ce9f704a22452b433e86d6ed764b389890e8338e
Gerardo Custodio <everythingissofast@gmail.com>
2026-06-06 17:53:02 -0600

    first commit moving the row in a list view swift

---
+++ move_row_in_list_view/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  move_row_in_list_view
+//
+//  Created by Gerardo J C on 06/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ move_row_in_list_view/move_row_in_list_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  move_row_in_list_viewApp.swift
+//  move_row_in_list_view
+//
+//  Created by Gerardo J C on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct move_row_in_list_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
